e5eea3facf16a6817f25b913e52158f76af187d2
Ebin008 <76677091+Ebin008@users.noreply.github.com>
2021-01-11 20:40:14 +0530

    Add files via upload

---
+++ Count the occurances.py
@@ -0,0 +1,12 @@
+#count the occurrences of each word in a line of text.
+line=input("enter the line")
+counts={}
+sentence=line.split()
+for word in sentence:
+    if word in counts:
+        counts[word]+=1
+    else:
+            counts[word]=1
+for k,v in counts.items():
+                print(k,v)
+                    
